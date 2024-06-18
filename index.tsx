e438655302c35b158f78ac953849758e1a1138e4
Kristaps Taube <t.kristaps@gmail.com>
2024-06-18 13:36:45 +0200

    pass config

--- index.tsx
+++ index.tsx
@@ -1,8 +1,37 @@
 import React from "react";
 import { App } from "./src/App.js";
 import { withFullScreen } from "./src/components/withFullScreen.js";
+import type { Config } from "./src/types.js";
 
-const ink = withFullScreen(<App />, { exitOnCtrlC: true });
+import { parse } from "path";
+
+function readFlag<V extends string | null>(flag: string, defaultValue: V): V {
+  const flagIndex = process.argv.indexOf(flag);
+  if (flagIndex !== -1) {
+    return parse(process.argv[flagIndex + 1]).dir as V;
+  } else {
+    return defaultValue;
+  }
+}
+
+const directoryPath = readFlag("-d", process.cwd());
+console.log(`Directory path specified: ${directoryPath}`);
+
+const helpFlag = process.argv.includes("--help");
+if (helpFlag) {
+  console.log("Usage: node index.tsx [-d <directory_path>]");
+  console.log("Options:");
+  console.log(
+    "  -d <directory_path>  Specify the directory path to be used by the application."
+  );
+  process.exit(0);
+}
+
+const config: Config = {
+  rootPath: directoryPath,
+};
+
+const ink = withFullScreen(<App config={config} />, { exitOnCtrlC: true });
 
 ink.start();
 await ink.waitUntilExit();
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { Box, Text } from "ink";
 import { useEffect, useState } from "react";
+import type { Config } from "./types.js";
 
-export const App = () => {
+interface Props {
+  config: Config;
+}
+
+export const App = ({ config }: Props) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
@@ -15,7 +20,7 @@ export const App = () => {
   }, []);
 
   return (
-    <Box height="100%" width="100%" borderStyle="single">
+    <Box height="100%" width="100%" borderStyle="round">
       <Text color="green">{counter} tests passed</Text>
     </Box>
   );
